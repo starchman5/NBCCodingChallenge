e317a3b74ca0a54215eee6af39f1785f6080cd74
Travis Stephens <46875447+starchman5@users.noreply.github.com>
2022-11-22 15:04:06 -0500

    Delete TravisStephensIOCContainer directory

--- TravisStephensIOCContainer/app/src/main/java/com/example/travisstephensioccontainer/ClassA.kt
+++
@@ -1,6 +0,0 @@
-package com.example.travisstephensioccontainer
-
-interface ClassA {
-    fun printClassBName()
-    fun printClassAName()
-}
--- TravisStephensIOCContainer/app/src/main/java/com/example/travisstephensioccontainer/ClassB.kt
+++
@@ -1,6 +0,0 @@
-package com.example.travisstephensioccontainer
-
-interface ClassB {
-    fun printClassAName()
-    fun printClassBName()
-}
